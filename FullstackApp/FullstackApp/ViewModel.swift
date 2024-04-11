e35e9469af6abcf32c9c512449719851214d13d4
maxwell-wayne-17 <mwayne@alarm.com>
2024-04-11 17:26:04 -0400

    Each button now fetches its own text

--- FullstackApp/FullstackApp/ViewModel.swift
+++ FullstackApp/FullstackApp/ViewModel.swift
@@ -14,51 +14,77 @@ class ViewModel: ObservableObject {
     private let currentIp = IP.home.rawValue
     
     func fetchTextOne() {
-        Task { @MainActor in
-            var displayText: String
-            do {
-                displayText = try await self.requestTextOne().text
-            }
-            catch NetworkError.invalidURL {
-                displayText = "Invalid URL"
-            } catch NetworkError.invalidResponse {
-                displayText = "Invalid response"
-            } catch NetworkError.invalidData {
-                displayText = "Invalid data"
-            } catch {
-                displayText = "Error"
-            }
-            self.textToDisplay = displayText
-        }
-    }
-    
-    private func requestTextOne() async throws -> TextModel {
-        let endpoint = "http://\(currentIp):8081/getall"
-        guard let url = URL(string: endpoint) else { throw NetworkError.invalidURL }
-        
-        let (data, response) = try await URLSession.shared.data(from: url)
-        
-        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else { throw NetworkError.invalidResponse }
-        
         do {
-            let decoder = JSONDecoder()
-            let texts = try decoder.decode([TextModel].self, from: data)
-            return texts[0]
+            try self.requestTextFor(id: "66158aa164b8f022a16c666e") { text in
+                DispatchQueue.main.async {
+                    self.textToDisplay = text.text
+                }
+            }
         } catch {
-            throw NetworkError.invalidData
+            self.textToDisplay = self.getErrorText(for: error)
         }
     }
     
     func fetchTextTwo() {
-        self.textToDisplay = "Text two"
+        do {
+            try self.requestTextFor(id: "66184c7241eed97a069d64e0") { text in
+                DispatchQueue.main.async {
+                    self.textToDisplay = text.text
+                }
+            }
+        } catch {
+            self.textToDisplay = self.getErrorText(for: error)
+        }
     }
     
     func fetchTextThree() {
-        self.textToDisplay = "Text three"
+        do {
+            try self.requestTextFor(id: "66184c7641eed97a069d64e2") { text in
+                DispatchQueue.main.async {
+                    self.textToDisplay = text.text
+                }
+            }
+        } catch {
+            self.textToDisplay = self.getErrorText(for: error)
+        }
     }
     
     func resetText() {
         self.textToDisplay = ViewModel.defaultText
+    }
+    
+    private func requestTextFor(id: String, completionHandler: @escaping(TextModel)->()) throws {
+        Task {
+            let endpoint = "http://\(currentIp):8081/get"
+            guard let url = URL(string: endpoint) else { throw NetworkError.invalidURL }
+            var request = URLRequest(url: url)
+            request.setValue(id, forHTTPHeaderField: "_id")
+            request.httpMethod = "GET"
+            
+            let (data, response) = try await URLSession.shared.data(for: request)
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else { throw NetworkError.invalidResponse }
+            
+            do {
+                let decoder = JSONDecoder()
+                let text = try decoder.decode(TextModel.self, from: data)
+                completionHandler(text)
+            } catch {
+                throw NetworkError.invalidData
+            }
+        }
+    }
+    
+    private func getErrorText(for error: Error) -> String {
+        guard let networkError = error as? NetworkError else { return "Unknown error" }
+        switch networkError {
+        case .invalidURL:
+            return "Invalid URL"
+        case .invalidResponse:
+            return "Invalid response"
+        case .invalidData:
+            return "Invalid data"
+        }
     }
     
 }
