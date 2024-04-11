1e5e0b1bb5ef8f14ce681aa0cb3c8d49d796d0fd
maxwell-wayne-17 <mwayne@alarm.com>
2024-04-11 16:14:58 -0400

    Successfully fetched text from database

---
+++ FullstackApp/FullstackApp/NetworkError.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkError.swift
+//  FullstackApp
+//
+//  Created by Maxwell Wayne on 4/10/24.
+//
+
+enum NetworkError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+}
--- FullstackApp/FullstackApp/ViewModel.swift
+++ FullstackApp/FullstackApp/ViewModel.swift
@@ -11,8 +11,42 @@ class ViewModel: ObservableObject {
     private static let defaultText = "Try fetching some text!"
     @Published var textToDisplay: String = ViewModel.defaultText
     
-    func fetchTextOne() async throws {
-        self.textToDisplay = "Text one"
+    private let currentIp = IP.home.rawValue
+    
+    func fetchTextOne() {
+        Task { @MainActor in
+            var displayText: String
+            do {
+                displayText = try await self.requestTextOne().text
+            }
+            catch NetworkError.invalidURL {
+                displayText = "Invalid URL"
+            } catch NetworkError.invalidResponse {
+                displayText = "Invalid response"
+            } catch NetworkError.invalidData {
+                displayText = "Invalid data"
+            } catch {
+                displayText = "Error"
+            }
+            self.textToDisplay = displayText
+        }
+    }
+    
+    private func requestTextOne() async throws -> TextModel {
+        let endpoint = "http://\(currentIp):8081/getall"
+        guard let url = URL(string: endpoint) else { throw NetworkError.invalidURL }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else { throw NetworkError.invalidResponse }
+        
+        do {
+            let decoder = JSONDecoder()
+            let texts = try decoder.decode([TextModel].self, from: data)
+            return texts[0]
+        } catch {
+            throw NetworkError.invalidData
+        }
     }
     
     func fetchTextTwo() {
